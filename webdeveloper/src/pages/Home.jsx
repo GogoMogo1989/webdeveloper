32d1ccb6aedcb9874c77837ee42424553fcfe352
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2025-07-11 17:52:45 +0200

    new build

--- webdeveloper/src/pages/Home.jsx
+++ webdeveloper/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import { gsap } from "gsap";
 import { Link } from "react-scroll";
 import { Helmet } from "react-helmet-async";
 import { useLanguage } from "../context/languageContext";
-import planet1 from "../assets/planetone (3).webp";
-import planet2 from "../assets/planettwo (3).webp";
-import stars from "../assets/planetthree (3).webp";
+import planet1 from "../assets/planetone.webp";
+import planet2 from "../assets/planettwo.webp";
+import stars from "../assets/planetthree.webp";
 import { useGSAP } from "@gsap/react";
 import video from "../assets/howtousecannon.mp4";
 
