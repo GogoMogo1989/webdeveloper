052f932ad2e1ba81d8770eda4342ffd81eaebf94
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2025-07-14 13:34:41 +0200

    new build

--- webdeveloper/src/components/CookieConset.jsx
+++ webdeveloper/src/components/CookieConset.jsx
@@ -14,6 +14,22 @@ const CookieConsent = ({ onAccept }) => {
     localStorage.setItem("cookieConsent", "true");
     setShow(false);
     onAccept();
+
+    const gtmScript = document.createElement("script");
+    gtmScript.src =
+      "https://www.googletagmanager.com/gtag/js?id=AW-17330617569";
+    gtmScript.async = true;
+    document.head.appendChild(gtmScript);
+
+    // Inline config script
+    const configScript = document.createElement("script");
+    configScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-17330617569');
+  `;
+    document.head.appendChild(configScript);
   };
 
   if (!show) return null;
@@ -22,8 +38,8 @@ const CookieConsent = ({ onAccept }) => {
     <div className="fixed bottom-0 left-0 w-full bg-black text-white p-4 flex flex-col md:flex-row justify-between items-center z-100">
       <p className="mb-2 md:mb-0 max-w-xl">
         {language === "hu"
-          ? "Ez az oldal YouTube videókat használ, amelyek sütiket helyeznek el. Kérjük, fogadd el a sütik használatát a videók lejátszásához."
-          : "This site uses YouTube videos that place cookies. Please accept the use of cookies to play the videos."}
+          ? "Ez a weboldal sütiket használ a felhasználói élmény javítása érdekében. Kérjük, fogadd el a sütik használatát a folytatáshoz."
+          : "This website uses cookies to enhance your user experience. Please accept the use of cookies to continue."}
       </p>
       <button onClick={acceptCookies} className="button-33">
         {language === "hu" ? "Elfogadom" : "Accept"}
