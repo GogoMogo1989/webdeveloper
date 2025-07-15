599a07cc72167bd5cee311f6736b23ae58c985a5
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2025-07-15 22:44:52 +0200

    npm run build

--- webdeveloper/src/pages/BlogDetail.jsx
+++ webdeveloper/src/pages/BlogDetail.jsx
@@ -58,7 +58,7 @@ const BlogDetail = () => {
         <SwiperSlide key={blog._id}>
           <div className="min-h-screen bg-white text-black py-10 px-6 flex flex-col items-center">
             <div
-              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 pointer-events-none select-none"
+              className="fixed top-25 left-1/2 -translate-x-1/2 z-50 pointer-events-none select-none"
               style={{ width: "420px", height: "330px" }}
             >
               <Lottie
